729fcc60ebe37a81fbb8271230380ef6e8f9b3f4
Petr Kovář <pejuko@gmail.com>
2025-03-01 15:28:58 +0100

    convert multiple languages script

---
+++ convert-wiki.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+BASE_DIR="data/kindle-wiki"
+TIMESTAMP="20250201"
+
+convert() {
+  local SOURCE_LANGUAGE=$1
+  local TARGET_LANGUAGE=$2
+  local PATTERN=$3
+
+  local DIR="$BASE_DIR/$SOURCE_LANGUAGE-$TARGET_LANGUAGE/$TIMESTAMP"
+
+  cargo run --release --\
+    -o $DIR \
+    -f \
+    -w data/enwiktionary-$TIMESTAMP-pages-articles.xml.bz2 -wp "$PATTERN" \
+    -t "English-$PATTERN Dictionary (pejuko)" \
+    -sl $SOURCE_LANGUAGE \
+    -tl $TARGET_LANGUAGE \
+    -a pejuko \
+    && \
+    wine ~/.wine/drive_c/users/pejuko/Local\ Settings/Application\ Data/Amazon/Kindle\ Previewer\ 3/lib/fc/bin/kindlegen.exe -c1 -gen_ff_mobi7 -dont_append_source "$DIR/content.opf"
+
+  if [ -f "$DIR/content.mobi" ]; then
+    mv "$DIR/content.mobi" "$DIR/$SOURCE_LANGUAGE-$TARGET_LANGUAGE-pejuko-$TIMESTAMP.mobi"
+  fi
+}
+
+convert en cs Czech
+convert en de German
+convert en es Spanish
+
